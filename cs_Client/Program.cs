12589ff2d6ef2d0283ec86d2c0a6aa90f60cdfaf
t.novruzov <t.novruzov@STHQ0118-01>
2022-02-27 21:53:22 +0400

    Add project files.

---
+++ cs_Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 45678;
+            var endPoint = new IPEndPoint(ip, port);
+
+            try
+            {
+                client.Connect(endPoint);
+
+                if (client.Connected)
+                {
+                    Console.WriteLine("Connected to the server...");
+
+                    Task.Run(() =>
+                    {
+                        var bytes = new byte[1024];
+                        var len = 0;
+                        var msg = "";
+
+                        while (true)
+                        {
+                            len = client.Receive(bytes);
+                            msg = Encoding.Default.GetString(bytes, 0, len);
+                            Console.WriteLine(msg);
+                        }
+
+                    });
+
+
+                    while (true)
+                    {
+                        var msg = Console.ReadLine();
+                        var bytes = Encoding.Default.GetBytes(msg);
+                        client.Send(bytes);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Can not connect to the server...");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EX: Can not connect to the server...");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ cs_Server/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cs_Server
+{
+    class Program
+    {
+        readonly static List<Socket> clients = new List<Socket>();
+        static void Main(string[] args)
+        {
+            using var listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            // 10.1.18.4
+            var ip = IPAddress.Parse("10.1.18.4");
+            var port = 45678;
+
+            var ep = new IPEndPoint(ip, port);
+
+            listener.Bind(ep);
+
+            var backlog = 1;
+            listener.Listen(backlog);
+
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine($"Listening on {listener.LocalEndPoint}");
+                var client = listener.Accept();
+                clients.Add(client);
+                Console.WriteLine($"{client.RemoteEndPoint} connected...");
+
+
+                Task.Run(() =>
+                {
+                   var consoleColor = (ConsoleColor)((client.RemoteEndPoint as IPEndPoint).Port % 15);
+
+                    var bytes = new byte[1024];
+                    var len = 0;
+                    var msg = "";
+
+                    while (true)
+                    {
+                        len = client.Receive(bytes);
+                        msg = Encoding.Default.GetString(bytes, 0, len);
+
+                        if (msg == "exit")
+                        {
+                            client.Shutdown(SocketShutdown.Both);
+                            client.Close();
+                            break;
+                        }
+
+                        msg = $"{client.RemoteEndPoint} : {msg}";
+                        
+                        Console.ForegroundColor = consoleColor;
+                        Console.WriteLine(msg);
+
+                        foreach (var c in clients)
+                        {
+                            if(client!= c)
+                            {
+                                c.Send(Encoding.Default.GetBytes(msg));
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
